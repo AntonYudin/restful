72e25006021a5478f4cc34c3cdd7d06c0f3e67d7
Anton Yudin <dev@antonyudin.com>
2024-03-23 23:00:40 -0400

    added launch*.sh files

---
+++ go/launch-server.sh
@@ -0,0 +1,20 @@
+
+
+rm key.store.p12
+
+keytool				\
+	-genkey			\
+	-keyalg rsa		\
+	-alias server		\
+	-keystore key.store.p12	\
+	-storepass 'test123'	\
+	-storetype PKCS12	\
+	-dname 'CN=localhost'
+
+openssl pkcs12 -info -in key.store.p12 -passin 'pass:test123' -nodes 
+
+openssl pkcs12 -in key.store.p12 -passin 'pass:test123' -nokeys -nodes -out server.crt
+openssl pkcs12 -in key.store.p12 -passin 'pass:test123' -nocerts -nodes -out server.key
+
+./server
+
